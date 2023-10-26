06449228f793b355ff84f7118b5093c3009f7eda
Hélio Soares <heliohelio1@outlook.com>
2023-10-25 21:40:23 -0300

    feat: add eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,32 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'eslint-plugin-prettier'],
   rules: {
+    'comma-dangle': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-shadow': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/button-has-type': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
   },
-}
+};

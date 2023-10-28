e938c562540490850d6631209e60ab881029a625
Hélio Soares <heliohelio1@outlook.com>
2023-10-28 19:52:14 -0300

    feat: add user model

---
+++ src/interfaces/api/models/User.ts
@@ -0,0 +1,11 @@
+import { UserRole } from '../../../types/global/UserRole';
+
+export interface UserDTO {
+  created_at: string;
+  email: string;
+  id: string;
+  last_name: string;
+  name: string;
+  type: UserRole;
+  updated_at: string;
+}
---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import { UserDTO } from '../interfaces/api/models/User';
+import { UserRole } from '../types/global/UserRole';
+
+class User {
+  createdAt!: string;
+
+  email!: string;
+
+  id!: string;
+
+  lastName!: string;
+
+  name!: string;
+
+  type!: UserRole;
+
+  updatedAt!: string;
+
+  constructor(data: User) {
+    Object.assign(this, { ...data });
+  }
+
+  static create(data: UserDTO) {
+    return new User({
+      createdAt: data?.created_at,
+      email: data?.email,
+      id: data?.id,
+      lastName: data?.last_name,
+      name: data?.name,
+      type: data?.type,
+      updatedAt: data?.updated_at,
+    });
+  }
+}
+
+export default User;
